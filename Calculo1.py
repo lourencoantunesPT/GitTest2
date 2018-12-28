c85d30a9199da9a9c1e73e2d49e13fbf89335c00
Lourenço ABC <lourenco.antunes.pt@gmail.com>
2018-12-28 06:15:27 +0000

    several calculations float

--- Calculo1.py
+++ Calculo1.py
@@ -1,6 +1,19 @@
-
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
 
 # Calculos básicos
 x = 7/3
 print(x)
+
+print('\n2+2=')
+print(2 + 2)
+
+print('\n50 - 5*6=')
+print(50 - 5*6)
+
+print('\n(50 - 5*6) / 4=')
+print((50 - 5*6) / 4)
+
+print('\n8 / 5=')
+print(8 / 5)  # division always returns a floating point number
 
